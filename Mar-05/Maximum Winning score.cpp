a7ab0bb557f65a014206fe901a64919e48098153
Sree Sayi Hrudai <63182223+hrudai2002@users.noreply.github.com>
2022-03-05 20:55:03 +0530

    Maximum Winning score ✅

---
+++ Mar-05/Maximum Winning score.cpp
@@ -0,0 +1,10 @@
+https://practice.geeksforgeeks.org/problems/4ead9c3991a3822f578309e2232bc5415ac35cb9/1#  
+
+long long findMaxScore(Node* root)
+    {
+        if(root) {
+            return root->data * max(findMaxScore(root->left), findMaxScore(root->right));
+        }
+        return 1;
+    }
+};
