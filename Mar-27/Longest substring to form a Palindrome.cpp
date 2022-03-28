28969635de9cb6d9aa19e2d25331aa09fa4db1a5
Sree Sayi Hrudai <63182223+hrudai2002@users.noreply.github.com>
2022-03-28 16:31:21 +0530

    Longest substring to form a Palindrome ✅

---
+++ Mar-27/Longest substring to form a Palindrome.cpp
@@ -0,0 +1,21 @@
+https://practice.geeksforgeeks.org/problems/longest-substring-whose-character-rearranged-can-form-a-palindrome/1# 
+
+int longestSubstring(string S) {
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+        
+        int state = 0, res = 0;
+        for(int i=0; i<S.length(); i++) {
+            state ^= 1<<(S[i]-'a');
+            if(mp.find(state) != mp.end()) res = max(res, i-mp[state]);
+            else mp[state] = i;
+            
+            for(int k=0; k<26; k++) {
+                int mask = state^(1<<k);
+                if(mp.find(mask) != mp.end())
+                    res = max(res, i-mp[mask]);
+            }
+        }
+        
+        return res;
+    }
